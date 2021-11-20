22f01d82f9cd267415c7edcedd4f321b6a9253ad
remax <deronzier.remi@gmail.com>
2021-11-20 16:05:27 +0100

    add multiple passive views

--- src/deronzier/remi/cardgame/games/Games.java
+++ src/deronzier/remi/cardgame/games/Games.java
@@ -3,14 +3,34 @@ package deronzier.remi.cardgame.games;
 import deronzier.remi.cardgame.controller.GameController;
 import deronzier.remi.cardgame.model.DeckFactory;
 import deronzier.remi.cardgame.model.DeckFactory.DeckType;
+import deronzier.remi.cardgame.view.GameSwingPassiveView;
 import deronzier.remi.cardgame.view.GameSwingView;
+import deronzier.remi.cardgame.view.GameViewables;
 
 public class Games {
 
 	public static void main(String[] args) {
+		GameViewables views = new GameViewables();
+
 		GameSwingView gsv = new GameSwingView();
 		gsv.createAndShowGUI();
-		GameController gc = new GameController(DeckFactory.makeDeck(DeckType.Normal), gsv, new HighCardGameEvaluator());
+		views.addViewables(gsv);
+
+		for (int i = 0; i < 3; i++) {
+			GameSwingPassiveView passiveView = new GameSwingPassiveView();
+			passiveView.createAndShowGUI();
+
+			views.addViewables(passiveView);
+
+			try {
+				Thread.sleep(2500);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+
+		GameController gc = new GameController(DeckFactory.makeDeck(DeckType.Normal), views,
+				new HighCardGameEvaluator());
 		gc.run();
 	}
 }
